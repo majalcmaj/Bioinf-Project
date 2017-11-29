af4979b04c5928451fd8ad48053b771b8e31c515
Wojciech Skwierawski <wojtek.skwierawski@gmail.com>
2017-11-29 17:52:43 +0100

    Added parser and input text file

---
+++ parser.py
@@ -0,0 +1,29 @@
+import re
+import numpy as np
+
+def get_vector (file):
+
+    vec = file.readline();
+    vec = re.sub("actguACTGU",'',vec);
+    vec = vec.upper();
+    vec = vec.split('[')[1].split(']')[0];
+    vec = vec.split();
+    return vec;
+
+def get_matrix (file):
+    w, h = 5, 5;
+    Matrix = [[np.char for x in range(w)] for y in range(h)]
+    file.readline();
+    for i in range(0,5):
+        line = file.readline();
+        Matrix[i][:] = line.split();
+
+    Matrix = np.array(Matrix);
+    Matrix = Matrix.astype(int);
+    return Matrix;
+
+
+
+
+
+
