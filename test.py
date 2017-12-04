e48438981e9cec7a22fdafa60429285643b27447
Michał Ciesielski <majalcmaj@gmail.com>
2017-12-04 23:04:46 +0100

    Najmniejsza odl edycyjna

--- test.py
+++ test.py
@@ -1,38 +1,79 @@
-from Scripts.parser import get_matrix, get_vector;
-from enum import Enum
+from collections import namedtuple
+import numpy as np
 
-"""class Index(Enum):
-    A = 0
-    C = 1
-    T = 2
-    G = 3
-    U = 4"""
+from parser import get_matrix, get_vector
 
-
-ascii_to_index = {'A': 0,
-           'C': 1,
-           'T': 2,
-           'G': 3,
-           'U': 4,
-           }
-
-file = open("biola.txt",'r');
-u = get_vector(file);
-v = get_vector(file);
-weights = get_matrix(file);
+ASCII_TO_IDX = {
+    'A': 0,
+    'C': 1,
+    'T': 2,
+    'G': 3,
+    '-': 4
+}
 
 
-print(u,v,weights);
+def vec_to_idx_vec(vec):
+    return [ASCII_TO_IDX[char] for char in vec]
 
 
-w, h = len(u), len(v);
-transit = [[0 for x in range(w)] for y in range(h)]
+SPACE_INDEX = 4
+if __name__ == "__main__":
+    Node = namedtuple("Node", "weight isLeft isDiag isUp")
+    # m = MyStruct("foo", "bar", "baz")
 
-for y in range(h):
-    for x in range(w):
-        transit[y][x] = weights[ascii_to_index[u[x]]][ascii_to_index[v[y]]]
+    file = open("biola.txt", 'r')
+    u = vec_to_idx_vec(get_vector(file))
+    v = vec_to_idx_vec(get_vector(file))
 
-print(transit);
+    weights = get_matrix(file)
+
+    # print(u, v, weights)
+
+    w, h = len(u) + 1, len(v) + 1
+
+    costs = np.zeros([w, h], dtype=np.int32)
+    transitions = np.zeros([w, h, 3], dtype=np.bool_)
+
+    print(transitions.shape)
+
+    for x in range(1, w):
+        costs[x, 0] = costs[x-1, 0] + weights[SPACE_INDEX, u[x - 1]]
+
+    for y in range(1, h):
+        costs[0, y] = costs[0, y - 1] + weights[v[y - 1], SPACE_INDEX]
+
+    for y in range(1, h):
+        for x in range(1, w):
+            trans_costs = np.array([
+                costs[x, y-1] + weights[v[y - 1], SPACE_INDEX],  # top
+                costs[x - 1, y] + weights[SPACE_INDEX, u[x - 1]],  # left
+                costs[x - 1, y - 1] + weights[v[y - 1], u[x - 1]]  # diag
+                ], dtype=np.int32)
+            min_trans = np.min(trans_costs)
+
+            idxs = []
+            for idx in range(3):
+                if trans_costs[idx] == min_trans:
+                    idxs.append(idx)
+            transitions[x, y][idxs] = True
+            costs[x, y] = min_trans
+
+    print(costs)
+    #
+    # # graph = np.empty(((w + 1) * (h + 1)), dtype=np.int32)
+    # # graph.fill(np.iinfo(np.int32).max)
+    #
+    #
 
 
 
+    # for y in range(h):
+    #     for x in range(w):
+    #         #graph[x][y] = weights[ascii_to_index[u[x]]][ascii_to_index[v[y]]]
+    #         upValue   = weights[ascii_to_index(v[y])][ascii_to_index['U']] +
+    #         leftValue = weights[ascii_to_index(u[x])][ascii_to_index['U']]
+    #         diagValue = weights[ascii_to_index(v[y])][ascii_to_index[u[x]]]
+    #
+    #         graph[x][y] = Node()
+    #
+    # print(graph)
