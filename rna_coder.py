7fd0a94046b0a4aa8ef9eed2df2611bc6294a838
Pawel <pawel.leckii@gmail.com>
2017-12-08 22:01:07 +0100

    Wpisana mapa trojek kodonowych na aminokwachy

--- rna_coder.py
+++ rna_coder.py
@@ -1,18 +1,42 @@
-CODONS_TO_AMINO = { #U C A G  | spacja na trzy spacje (i odwrotnie)
-    "UUU" : "",
-
+CODONS_TO_AMINO = {  # U C A G  | spacja na trzy spacje (i odwrotnie)
+    "GCU": "A", " GCC": "A", " GCA": "A", " GCG": "A",
+    "UGU": "C", " UGC": "C",
+    "GAU": "D", " GAC": "D",
+    "GAA": "E", " GAG": "E",
+    "UUU": "F", " UUC": "F",
+    "GGU": "G", " GGC": "G", " GGA": "G", " GGG": "G",
+    "CAU": "H", " CAC": "H",
+    "AUU": "I", " AUC": "I", " AUA": "I",
+    "AAA": "K", " AAG": "K",
+    "UUA": "L", " UUG": "L", " CUU": "L", " CUC": "L", " CUA": "L", " CUG": "L",
+    "AUG": "M",
+    "AAU": "N", " AAC": "N",
+    "UAG": "O",
+    "CCU": "P", " CCC": "P", " CCA": "P", " CCG": "P",
+    "CAA": "Q", " CAG": "Q",
+    "CGU": "R", " CGC": "R", " CGA": "R", " CGG": "R", " AGA": "R", " AGG": "R",
+    "UCU": "S", " UCC": "S", " UCA": "S", " UCG": "S", " AGU": "S", " AGC": "S",
+    "ACU": "T", " ACC": "T", " ACA": "T", " ACG": "T",
+    "UGA": "U",
+    "GUU": "V", " GUC": "V", " GUA": "V", " GUG": "V",
+    "UGG": "W",
+    "UAU": "Y", " UAC": "Y",
+    "UAA": "-", " UAG": "-", " UGA": "-",
 }
 
-def rna_code(): #input array, return array
+
+def rna_code():  # input array, return array
     pass
     # wejscie: ciag trojek kodonowych ACTG, wyjscie: literki aminokwasow (tablica)
 
+
 def rna_decode():
     pass
-    #odwrotnie XD
+    # odwrotnie XD
 
-#zaistalowac interpreter pythona + numpy
-#python dictionairy {"A" : "wartosc" , "B" : "wartoscB"}
+
+# zaistalowac interpreter pythona + numpy
+# python dictionairy {"A" : "wartosc" , "B" : "wartoscB"}
 
 if __name__ == "__main__":
-    pass # tutaj testy, reszta plikow mnie nie interere wczyt,
+    pass  # tutaj testy, reszta plikow mnie nie interere wczyt,
