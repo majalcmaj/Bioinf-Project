f331e1373da4a0fac6be857c9d8c22f07efe8f26
Pawel <pawel.leckii@gmail.com>
2017-12-08 17:29:18 +0100

    Pawel zaczyna

---
+++ rna_coder.py
@@ -0,0 +1,18 @@
+CODONS_TO_AMINO = { #U C A G  | spacja na trzy spacje (i odwrotnie)
+    "UUU" : "",
+
+}
+
+def rna_code(): #input array, return array
+    pass
+    # wejscie: ciag trojek kodonowych ACTG, wyjscie: literki aminokwasow (tablica)
+
+def rna_decode():
+    pass
+    #odwrotnie XD
+
+#zaistalowac interpreter pythona + numpy
+#python dictionairy {"A" : "wartosc" , "B" : "wartoscB"}
+
+if __name__ == "__main__":
+    pass # tutaj testy, reszta plikow mnie nie interere wczyt,
