63322ca8cf2e67cfbb27873988b97af2818d527a
Michał Ciesielski <majalcmaj@gmail.com>
2017-12-09 15:28:12 +0100

    coder refactor

--- rna_coder.py
+++ rna_coder.py
@@ -1,4 +1,6 @@
-CODONS_TO_AMINO = {
+import itertools
+
+CODONS_AMIN_MAP = {
     "GCT": "A", "GCC": "A", "GCA": "A", "GCG": "A",
     "TGT": "C", "TGC": "C",
     "GAT": "D", "GAC": "D",
@@ -21,35 +23,21 @@ CODONS_TO_AMINO = {
     "GTT": "V", "GTC": "V", "GTA": "V", "GTG": "V",
     "TGG": "W",
     "TAT": "Y", "TAC": "Y",
-    "TAA": "|", "TAG": "|", "TGA": "|"
+    "TAA": "|", "TAG": "|", "TGA": "|",
+    "---": "-"
 }
 
-
-def rna_code(codons_array):  # input array of codons (len%3==0), return array of amino acids
-    aminos_array = []
-    codons_copy = codons_array  # do I need to do this? On the Internet they say I do
-    while len(codons_copy) >= 3:  # if there is 2 or less characters left just ignore them
-        amino = CODONS_TO_AMINO.get(codons_copy[:3])
-        # amino = (amino != None) ? amino : "!" # ????????? IN JAVA IT WORKS XD
-        if amino is not None:
-            aminos_array.append(amino)
-        else:
-            aminos_array.append("!")  # there was wrong codons triplet, write "!" instead of amino acid
-        codons_copy = codons_copy.replace(codons_copy[:3], '', 1)  # delete first 3 chars
-    return aminos_array
+AMIN_CODONS_MAP = {k: list(v)[0][0] for k, v in itertools.groupby(CODONS_AMIN_MAP.items(), lambda pair: pair[1])}
 
 
-def rna_decode(aminos_array):
-    aminos_copy = aminos_array  # do I need to copy input?
-    codons_array = []
-    while aminos_copy != '':
-        next_amino = aminos_copy[:1]
-        for found_codon_triplet, search_amino in CODONS_TO_AMINO.items():
-            if next_amino == search_amino:  # TODO: what if key not found , and special cases: "O" and "U"
-                codons_array.append(found_codon_triplet)
-                break
-        aminos_copy = aminos_copy.replace(aminos_copy[:1], '', 1)  # delete first character
-    return codons_array
+def rna_code(codons_array):
+    assert len(codons_array) % 3 == 0, "The given codons array has to have the length divisible by 3"
+    codon_triplets = ["".join(codons_array[idx:idx + 3]) for idx in range(0, len(codons_array), 3)]
+    return list(map(lambda codon: CODONS_AMIN_MAP[codon], codon_triplets))
+
+
+def rna_decode(amins_array):
+    return [AMIN_CODONS_MAP[amin] for amin in amins_array]
 
 
 if __name__ == "__main__":
@@ -57,7 +45,7 @@ if __name__ == "__main__":
     example_aminos = "ACDEFG"
 
     print(rna_code(example_codons))
-    print(len(example_codons)/3, len(rna_code(example_codons)))
+    print(len(example_codons) / 3, len(rna_code(example_codons)))
 
     print(rna_decode(example_aminos))
     print(len(example_aminos), len(rna_decode(example_aminos)))
